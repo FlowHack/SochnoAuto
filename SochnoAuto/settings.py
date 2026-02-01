989052194bd639a9e229c7bb160aad934bf4bec3
FlowHack <flow.hack.com@gmail.com>
2026-02-01 23:52:51 +0300

    Connect apps homepage and auto_store

--- SochnoAuto/settings.py
+++ SochnoAuto/settings.py
@@ -15,6 +15,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
+    'auto_store.apps.AutoStoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
