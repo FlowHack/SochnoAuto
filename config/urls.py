44871926df0878e02aab8e8ab09f7508e513b310
FlowHack <flow.hack.com@gmail.com>
2026-02-22 18:07:16 +0300

    Create path from ckeditor

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
