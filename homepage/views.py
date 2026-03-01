c5a814159f4315f9ac834969c61c48759553406a
FlowHack <flow.hack.com@gmail.com>
2026-03-01 21:39:00 +0300

    404 and 500 errors have been processed

--- homepage/views.py
+++ homepage/views.py
@@ -10,3 +10,16 @@ class IndexView(View, IndexService):
     def get(self, request):
         context = self.get_index_context(request)
         return render(request, 'homepage/index.html', context=context)
+
+
+def page_not_found(request, exception):
+    return render(
+        request,
+        'misc/404.html',
+        {'path': request.path},
+        status=404
+    )
+
+
+def server_error(request):
+    return render(request, 'misc/500.html', status=500)
