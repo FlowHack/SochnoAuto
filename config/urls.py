c4deae870e64c441db78aac2fec71c993b4b91af
FlowHack <flow.hack.com@gmail.com>
2026-03-26 04:18:06 +0300

    FIX: I001 isort found an import in the wrong position

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 handler404 = 'homepage.views.page_not_found'  # noqa
 handler500 = 'homepage.views.server_error'  # noqa
